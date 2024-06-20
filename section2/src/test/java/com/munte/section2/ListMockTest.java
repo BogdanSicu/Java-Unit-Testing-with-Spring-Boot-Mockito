c355fce2a50b421bd91b1270fb582a8005a9ab51
BogdanSicu <b0gdan.munt3@gmail.com>
2024-06-20 16:28:12 +0300

    added spy test

--- section2/src/test/java/com/munte/section2/ListMockTest.java
+++ section2/src/test/java/com/munte/section2/ListMockTest.java
@@ -3,6 +3,7 @@ package com.munte.section2;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -82,6 +83,42 @@ public class ListMockTest {
         List<String> allValues = captor.getAllValues();
         assertEquals("something1", allValues.get(0));
         assertEquals("something2", allValues.get(1));
+    }
+
+    @Test
+    public void compareMockToSpy() {
+
+        ArrayList arrayListMock = mock(ArrayList.class);
+
+        System.out.println(arrayListMock.get(0)); //null
+        System.out.println(arrayListMock.size()); //0
+        arrayListMock.add("Test");
+        arrayListMock.add("Test2");
+        System.out.println(arrayListMock.size()); //0
+        when(arrayListMock.size()).thenReturn(5);
+        System.out.println(arrayListMock.size()); //5
+
+    }
+
+    @Test
+    public void spying() {
+
+        ArrayList arrayListSpy = spy(ArrayList.class);
+
+        arrayListSpy.add("Test0");
+
+        System.out.println(arrayListSpy.get(0)); // Test0
+        System.out.println(arrayListSpy.size()); // 1
+        arrayListSpy.add("Test1");
+        arrayListSpy.add("Test2");
+        System.out.println(arrayListSpy.size()); // 3
+        when(arrayListSpy.size()).thenReturn(5);
+        System.out.println(arrayListSpy.size()); // 5
+
+        arrayListSpy.add("Test3");
+        System.out.println(arrayListSpy.size()); // 5
+
+        verify(arrayListSpy).add("Test3");
     }
 
 }
